96f41f52c566cf92b3ac22b5837132fd24d2f800
Peterzhu123 <Peter.zhu@ocsbstudent.ca>
2018-05-07 17:41:08 -0400

    Create main.java

---
+++ main.java
@@ -0,0 +1,11 @@
+// Create by : Peter zhu
+// Create on : May
+// This is mian class of Vehicle class
+public class VehicleMain {
+	public static void main(String[]args) {
+		Vehicle vehicle = new Vehicle(34554,4,50,120,"Black");
+		vehicle.speak();	
+		
+	}
+
+}
